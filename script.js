73e3f273852a61673ef5993475fc22ad7352e924
LackOfPtrl <96771702+LackOfPtrl@users.noreply.github.com>
2022-01-04 16:41:34 +0000

    new songs and update to js

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const musicContainer = document.getElementById('music-container');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
+
 const audio = document.getElementById('audio');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
 
 //Song titles
-const songs = ['song1'];
+const songs = ['song1','song2','song3'];
 
 //Song selected
 let songIndex = 0;
@@ -40,6 +45,36 @@ function pauseSong(){
     audio.pause();
 }
 
+//Next song function
+function nextSong(){
+    songIndex++
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+      }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+//Next song function
+function prevSong(){
+    songIndex--
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+      }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
 //Pause or play song
 playBtn.addEventListener('click', () =>{
     const isPlaying = musicContainer.classList.contains('play');
@@ -51,3 +86,31 @@ playBtn.addEventListener('click', () =>{
     }
 
 })
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
+// Time of song
+audio.addEventListener('timeupdate',DurTime);
+
+// Update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+
+  // Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+  
+    audio.currentTime = (clickX / width) * duration;
+  }
